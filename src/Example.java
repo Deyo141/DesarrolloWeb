31b2507562bdb65d91b7c16625455dd332c0ec31
saag5 <saag5@AresAce>
2021-08-09 14:52:16 -0500

    Nuevo intento 2.0

--- src/Example.java
+++ src/Example.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -10,11 +5,13 @@
  */
 public class Example {
 
-    /**
-     * @par am args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo XD");
+        System.out.println("");
+        System.out.println("");
+        
     }
     
 }
