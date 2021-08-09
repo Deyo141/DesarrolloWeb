f39cf4ba9dded541ff56f2ae9369064e71ced5a2
5AresAce6 <70976289+5AresAce6@users.noreply.github.com>
2021-08-09 13:12:48 -0500

    Update Main.java
    Actu

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Puto el que lo lea!");
+        System.out.println("Actualizacion!");
     }
     
 }
