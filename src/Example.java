0cf34cfb076e267474dd56615fcdcb09d28fe44a
saag5 <saag5@AresAce>
2021-08-09 14:57:11 -0500

    Nuevo mensaje

--- src/Example.java
+++ src/Example.java
@@ -4,13 +4,13 @@
  * @author saag5
  */
 public class Example {
-
+    
+    static String mensaje = "Nuevo mensaje";
 
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo XD");
-        System.out.println("");
-        System.out.println("");
+        System.out.println(mensaje);
         
     }
     
